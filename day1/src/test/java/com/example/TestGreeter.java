cc903b8bda143b6ab7ab62827455e1c93ab33a8a
Ovidiu Maja <ovi.dan89@gmail.com>
2017-08-31 12:33:15 +0300

    Initial commit - sample per day maven project

--- day1/src/test/java/com/example/TestGreeter.java
+++ day1/src/test/java/com/example/TestGreeter.java
@@ -3,10 +3,8 @@ package com.example;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
 
 public class TestGreeter {
 
